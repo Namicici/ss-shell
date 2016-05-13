3419ab64646d9432f3928d373ebda95c39bd8b08
Namicici <847954654@qq.com>
2016-05-13 17:06:35 +0800

    optimise code

--- src/app/routing.js
+++ src/app/routing.js
@@ -1,8 +1,6 @@
 "use strict";
 
 var app = require("./app.js");
-//var routers = require("./routers.js").routers;
-//var loader = require("./routers.js").loader;
 
 app.factory("authInterceptor", [function(){
     var authInterceptor = {};
@@ -11,63 +9,40 @@ app.factory("authInterceptor", [function(){
 
 app.config(["$routeProvider","$httpProvider", function($routeProvider, $httpProvider){
         $httpProvider.interceptors.push("authInterceptor");
-/*
-        angular.forEach(routers, function(router, path){
-            $routeProvider.when(path, {
-                name:router.name,
-                template: require(router.templateUrl),
-                controller:router.controller,
-                resolve: //loader(router.dependencies)
-                {
-                    loadController: function($q){
-                        var deffer = $q.defer();
-                        require.ensure([], function(require){
-                            for (var i = 0; i < router.dependencies.length; i++){
-                                require(router.dependencies);
-                            }
-                            deffer.resolve();
-                        })
-                        return deffer.promise;
-                    }
+
+        function loader(dependencies){
+            var load = {
+                loader: function($q){
+                    var deffer = $q.defer();
+                    dependencies(deffer);
+                    return deffer.promise;
                 }
-            })
-        })
-        */
+            };
+            return load;
+        }
+
         $routeProvider
         .when("/",{
             name: "home",
             template: require("./views/home/home.html"),
             controller:"app.views.home",
-            resolve: //loader(["./views/home/home.js"])
-            {
-                loadController: function($q){
-
-                    var deffer = $q.defer();
-                    require.ensure([], function(require){
-                        require("./views/home/home.js");
-                        deffer.resolve();
-                    })
-                    return deffer.promise;
-                }
-            }
+            resolve: loader(function(deffer){
+                require.ensure([], function(require){
+                    require("./views/home/home.js");
+                    deffer.resolve();
+                })
+            })
         })
         .when("/main",{
             name:"main",
             template: require("./views/main/main.html"),
             controller:"app.views.main",
-            resolve://loader(["./views/main/main.js"])
-            {
-                loadController: function($q){
-
-                    var deffer = $q.defer();
-                    require.ensure([], function(require){
-                        require("./views/main/main.js");
-                        deffer.resolve();
-                    })
-                    return deffer.promise;
-
-                }
-            }
+            resolve:loader(function(deffer){
+                require.ensure([], function(require){
+                    require("./views/main/main.js");
+                    deffer.resolve();
+                })
+            })
         })
 
     }])
